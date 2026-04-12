f0b13fc391fd83cc1de76a2142b7b3e19e8ac702
HuyennTran <huyentn.bg1@gmail.com>
2026-04-12 21:36:22 +0700

    Update code

--- data/Download_data.py
+++ data/Download_data.py
@@ -3,19 +3,28 @@ import librosa
 import soundfile as sf
 
 def download_and_prepare_dataset():
-    output_dir = os.path.join("data", "raw")
-    os.makedirs(output_dir, exist_ok=True)  
-    print(f"Folder: {output_dir}")
+    base_dir = os.path.join("data", "raw")
+    print(f"Base Folder: {base_dir}")
     print("-" * 40)
+    
     audio_samples = {
-        'speech_male.wav': 'choice',      
-        'speech_female.wav': 'libri1',   
+        'speech_male.wav': 'libri1',      
+        'speech_female.wav': 'libri2',   
         'music_trumpet.wav': 'trumpet',  
         'music_orchestra.wav': 'brahms'   
     }
+    
     target_sr = 44100  
+    
     for filename, librosa_key in audio_samples.items():
         print(f"Processing file: {filename} (from '{librosa_key}') ...")
+        
+        if filename.startswith('speech'):
+            output_dir = os.path.join(base_dir, 'speech')
+        else:
+            output_dir = os.path.join(base_dir, 'music')
+            
+        os.makedirs(output_dir, exist_ok=True)
         
         try:
             file_path = librosa.ex(librosa_key)
@@ -30,5 +39,5 @@ def download_and_prepare_dataset():
     print("-" * 40)
     print("FINISH!")
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     download_and_prepare_dataset()
